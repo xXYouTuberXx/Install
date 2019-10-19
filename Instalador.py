253cb7870bf6bd573156dbca1a7080078badda5a
xXYouTuberXx <53968295+xXYouTuberXx@users.noreply.github.com>
2019-10-19 16:54:17 -0500

    Add files via upload

---
+++ Instalador.py
@@ -0,0 +1,35 @@
+import os
+import colorama
+from colorama import Fore
+
+os.system("cls")
+
+print(Fore.CYAN + "Bienvenido A La Tool Para Descargar Python3")
+
+os.system("yum install centos-release-scl -y")
+
+os.system("clear")
+
+os.system("yum info rh-python35")
+
+os.system("clear")
+
+os.system("yum install rh-python35 -y")
+
+os.system("clear")
+
+os.system("scl enable rh-python35 bash")
+
+os.system("clear")
+
+os.system("scl -l")
+
+os.system("clear")
+
+os.system("pip3 install colorama")
+
+os.system("clear")
+
+os.system("pip3 install cfscrape")
+
+os.system("clear")
